95cd8b4b5af7aca25844c83e9467ba43f2cfeb7b
Raphael Alampay <raphael.alampay@gmail.com>
2023-04-17 13:54:35 +0800

    make response json data type

--- demo/src/main/java/com/companyx/demo/controllers/PositionsController.java
+++ demo/src/main/java/com/companyx/demo/controllers/PositionsController.java
@@ -4,15 +4,19 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.companyx.demo.services.PositionsService;
+import com.google.gson.Gson;
 
 @RestController
-@RequestMapping("/api/positions")
+@ResponseBody
+@RequestMapping(path={"/api/positions"}, produces=MediaType.APPLICATION_JSON_VALUE)
 public class PositionsController {
 
     @Autowired
@@ -24,7 +28,10 @@ public class PositionsController {
     public String index() {
         List<HashMap<String, Object>> items = positionsService.getAll();
 
-        return "Number of positions: " + items.size();
+        Gson gson = new Gson();
+        String payload = gson.toJson(items);
+
+        return payload;
     }
 
     // Endpoint to return a single position given an id
@@ -33,14 +40,30 @@ public class PositionsController {
     public String show(@PathVariable int id) {
         HashMap<String, Object> item = positionsService.getById(id);
 
-        return "A single position with id " + id + " and name " + item.get("name");
+        Gson gson = new Gson();
+        String payload = gson.toJson(item);
+
+        return payload;
     }
+
+    // Morning Exercise:
+    // Implement the create method and delete method of positionService and call it in its respective controller endpoints
 
     // Enpoint to add a new position
     // POST /api/positions
     @RequestMapping(value={"", "/"}, method=RequestMethod.POST)
     public String create() {
-        return "Creating new position";
+        int newId = 3;
+        String newName = "New Position";
+
+        HashMap<String, Object> item = new HashMap<String, Object>();
+        item.put("id", newId);
+        item.put("name", newName);
+
+        Gson gson = new Gson();
+        String payload = gson.toJson(item);
+
+        return payload;
     }
 
     // Endpoint to edit a position
@@ -54,6 +77,16 @@ public class PositionsController {
     // DELETE /api/positions/:id
     @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
     public String delete(@PathVariable int id) {
-        return "Deleting position with id " + id;
+        /*
+         * { code: 101, message: "successfully deleted" }
+         */
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        result.put("code", 101);
+        result.put("message", "Successfully deleted");
+
+        Gson gson = new Gson();
+        String payload = gson.toJson(result);
+
+        return payload;
     }
 }
